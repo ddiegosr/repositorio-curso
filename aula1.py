2f10166ef86de47c2ac19a6851dc403d48dd6a8a
Edson Lengruber <edsondpaula3945@gmail.com>
2023-01-11 20:03:19 -0300

    edinho modificação

--- aula1.py
+++ aula1.py
@@ -1,2 +1,10 @@
 print('Outro')
-print('dasdsa')
+print('outro 1')
+# digite 3 números e aponte o maior número
+
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite o segundo número: '))
+n2 = float(input('Digite o terceiro número: '))
+
+if n1 > n2 and n1 > n3
+    print('O maior número é:{}')
